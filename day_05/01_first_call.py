66ee55513aa820c4fc4df85ab3e14dc7f673df6d
Ruthran Raghavan <ruthran.raghavan@tellestia.com>
2026-08-08 21:55:57 +0530

    first_call

--- day_05/01_first_call.py
+++ day_05/01_first_call.py
@@ -2,4 +2,18 @@ import os
 from openai import OpenAI
 from dotenv import load_dotenv
 
+load_dotenv()
 
+client = OpenAI(base_url=os.environ["BASE_URL"], api_key=os.environ["OPENROUTER_API_KEY"])
+
+MODEL = os.environ["MODEL"]
+
+response = client.chat.completions.create(
+    model=MODEL,
+    messages=[
+        {"role": "system", "content": "You are a Tax Consultant assistant. Always answer in a few lines."},
+        {"role": "user", "content": "How can i reduce my tax liability"}
+    ]
+)
+
+print("Caramel AI: ", response.choices[0].message.content)
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from generative-ai-professional-certification-eduhubspot-s2!")
+
+
+if __name__ == "__main__":
+    main()
